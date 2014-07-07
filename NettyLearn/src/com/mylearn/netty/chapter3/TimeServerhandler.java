661c71d14e1b369050c7ebd91273449b91123a83
snakeek <skywalker124@gmail.com>
2014-07-07 23:55:19 +0800

    netty definitive guide chapter 3

---
+++ NettyLearn/src/com/mylearn/netty/chapter3/TimeServerhandler.java
@@ -0,0 +1,35 @@
+package com.mylearn.netty.chapter3;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class TimeServerhandler extends ChannelHandlerAdapter {
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		ByteBuf buf = (ByteBuf) msg;
+		byte[] req = new byte[buf.readableBytes()];
+		buf.readBytes(req);
+		String body = new String(req, "UTF-8");
+		System.out.println("The time Server receive order : " + body);
+		String currentTime = "Query Time Order".equalsIgnoreCase(body) ? new java.util.Date(
+				System.currentTimeMillis()).toString() : "Bad Order";
+		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+		ctx.write(resp);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
+}
--- NettyLearn/src/com/mylearn/netty/client/SecondNettyClientHandler.java
+++ NettyLearn/src/com/mylearn/netty/client/SecondNettyClientHandler.java
@@ -1,20 +1,12 @@
 package com.mylearn.netty.client;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.CharsetUtil;
 
 public class SecondNettyClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
-
-	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg)
-			throws Exception {
-		System.out.println("Client Received : "
-				+ ByteBufUtil.hexDump(msg.readBytes(msg.readableBytes())));
-	}
 
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -26,6 +18,13 @@ public class SecondNettyClientHandler extends SimpleChannelInboundHandler<ByteBu
 			throws Exception {
 		cause.printStackTrace();
 		ctx.close();
+	}
+
+	@Override
+	protected void messageReceived(ChannelHandlerContext ctx, ByteBuf msg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
