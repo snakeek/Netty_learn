46729e9cd150fa02a410f47aeb1a331f204d0b74
snakeek <skywalker124@gmail.com>
2014-07-08 21:57:47 +0800

    add test code

---
+++ NettyLearn/src/com/mylearn/netty/guide/chapter3/TimeServer.java
@@ -0,0 +1,48 @@
+package com.mylearn.netty.guide.chapter3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+
+	public void bind(int port) throws Exception {
+
+		EventLoopGroup boss = new NioEventLoopGroup();
+		EventLoopGroup worker = new NioEventLoopGroup();
+
+		try {
+			ServerBootstrap boot = new ServerBootstrap();
+			boot.group(boss, worker)
+				.channel(NioServerSocketChannel.class)
+				.option(ChannelOption.SO_BACKLOG, 1024)
+				.childHandler(new ChildChannelHandler());
+			ChannelFuture future = boot.bind(port).sync();
+
+			future.channel().closeFuture().sync();
+		} finally {
+			boss.shutdownGracefully();
+			worker.shutdownGracefully();
+		}
+	}
+
+	private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+		@Override
+		protected void initChannel(SocketChannel ch) throws Exception {
+			ch.pipeline().addLast(new TimeServerHandler());
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		int port = 8080;
+
+		new TimeServer().bind(port);
+	}
+}
