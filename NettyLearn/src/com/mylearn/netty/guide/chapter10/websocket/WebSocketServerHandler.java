efbcd03987f4ed50c7de8951c0a497cb4e0f9fb6
snakeek <skywalker124@gmail.com>
2014-07-15 23:25:14 +0800

    learn websocket

--- NettyLearn/src/com/mylearn/netty/guide/chapter10/websocket/WebSocketServerHandler.java
+++ NettyLearn/src/com/mylearn/netty/guide/chapter10/websocket/WebSocketServerHandler.java
@@ -1,16 +1,32 @@
 package com.mylearn.netty.guide.chapter10.websocket;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpConstants;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
 import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
+import io.netty.util.CharsetUtil;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.xml.ws.spi.http.HttpContext;
+import javax.xml.ws.spi.http.HttpHandler;
 
 public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
 
@@ -31,13 +47,12 @@ public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> 
 
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-		super.channelReadComplete(ctx);
+		ctx.flush();
 	}
 
 	private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
 		if (!req.getDecoderResult().isSuccess() || (!"websocket".equals(req.headers().get("Upgrade")))) {
-			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, BAD_REQUEST));
+			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
 			return;
 		}
 		
@@ -53,9 +68,49 @@ public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> 
 	}
 	
 	private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
+		//判断是否是关闭链路的指令
 		if (frame instanceof CloseWebSocketFrame) {
 			handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
 			return;
 		}
+		//判断是否是PING消息
+		if (frame instanceof PingWebSocketFrame) {
+			ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+		}
+		//只支持文本消息，不支持二进制消息
+		if (!(frame instanceof TextWebSocketFrame)) {
+			throw new UnsupportedOperationException(String.format(
+					"%s frame types not supported", frame.getClass().getName()));
+		}
+		//返回应答消息
+		String request = ((TextWebSocketFrame) frame).text();
+		if (logger.isLoggable(Level.FINE)) {
+			logger.fine(String.format("%s received %s", ctx.channel(), request));
+		}
+		ctx.channel().write(
+				new TextWebSocketFrame(request + " , 欢迎使用Netty Websocket服务，现在时刻: "
+						+ new java.util.Date().toString()));
+	}
+	
+	private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse res) throws Exception {
+		//返回应答给客户端
+		if (res.getStatus().code() != 200) {
+			ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(), CharsetUtil.UTF_8);
+			res.content().writeBytes(buf);
+			buf.release();
+			//setContentLength(res, res.content().readableBytes());
+		}
+		
+		ChannelFuture f = ctx.channel().writeAndFlush(res);
+		if (!HttpHeaders.isKeepAlive(req) || res.getStatus().code() != 200) {
+			f.addListener(ChannelFutureListener.CLOSE);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		cause.printStackTrace();
+		ctx.close();
 	}
 }
